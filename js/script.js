4eb75a5f31a038f15b7f8b5979e6a2374708de5d
henriquericha45 <henriquericha@hotmail.com>
2022-08-10 06:20:28 -0300

    progresso na pagina de log

---
+++ js/script.js
@@ -0,0 +1,23 @@
+
+//const input = document.querySelector('.login');
+//const button = document.querySelector('.button-play');
+//const form = document.querySelector('.login-form');
+
+//const validateInput = ({ target }) => {
+//  if (target.value.length > 2) {
+//    button.removeAttribute('disabled');
+//    return;
+//  }
+//
+//  button.setAttribute('disabled', '');
+//}
+//
+//const handleSubmit = (event) => {
+//  event.preventDefault();
+//  
+//  localStorage.setItem('player', input.value);
+//  window.location = 'paginas/jogo.html';
+//}
+//
+//input.addEventListener('input', validateInput);
+//form.addEventListener('submit', handleSubmit);
