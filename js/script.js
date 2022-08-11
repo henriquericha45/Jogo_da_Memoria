988a7406034651071c444544fa9a220717006962
henriquericha45 <henriquericha@hotmail.com>
2022-08-11 05:20:52 -0300

    atualizacao

--- js/script.js
+++ js/script.js
@@ -1,23 +1,47 @@
-
 //const input = document.querySelector('.login');
 //const button = document.querySelector('.button-play');
 //const form = document.querySelector('.login-form');
 
 //const validateInput = ({ target }) => {
-//  if (target.value.length > 2) {
+//  if (input.value != null) {
 //    button.removeAttribute('disabled');
 //    return;
 //  }
-//
+
 //  button.setAttribute('disabled', '');
 //}
-//
+
 //const handleSubmit = (event) => {
 //  event.preventDefault();
-//  
+  
 //  localStorage.setItem('player', input.value);
 //  window.location = 'paginas/jogo.html';
 //}
-//
+
 //input.addEventListener('input', validateInput);
-//form.addEventListener('submit', handleSubmit);
+//button.addEventListener('submit', handleSubmit);
+
+const input = document.querySelector('.login-input');
+const button = document.querySelector('.login-button');
+const form = document.querySelector('.login-form');
+
+const validarInput = ({target}) => {
+    if(target!=""){
+        console.log('foi');
+        button.removeAttribute('disabled', '');
+        return;
+    }
+    button.setAttribute('disable', '');
+}
+
+input.addEventListener('button', validarInput);
+
+function valida(){
+    document.getElementsByClassName('login-button').disabled = true;
+    if(document.getElementsByClassName('login-input').value != ""){
+        document.getElementsByClassName('login-button').disabled = false;
+
+    }else{
+        console.log('erro');
+    }
+}
